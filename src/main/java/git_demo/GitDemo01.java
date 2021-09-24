30b96972457ebec0f7a5a336993a9a4b3fc0e064
ethene <lunlixwyx@163.com>
2021-09-24 21:07:56 +0800

    third commit

--- src/main/java/git_demo/GitDemo01.java
+++ src/main/java/git_demo/GitDemo01.java
@@ -3,5 +3,6 @@ package git_demo;
 public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("second version");
+        System.out.println("third version");
     }
 }
