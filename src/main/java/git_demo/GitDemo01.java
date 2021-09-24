7c94518a9b27f7d94cabe4ddc953f19b3e91ad5d
ethene <lunlixwyx@163.com>
2021-09-24 21:19:06 +0800

    fourth commit

--- src/main/java/git_demo/GitDemo01.java
+++ src/main/java/git_demo/GitDemo01.java
@@ -4,5 +4,6 @@ public class GitDemo01 {
     public static void main(String[] args) {
         System.out.println("second version");
         System.out.println("third version");
+        System.out.println("fourth version");
     }
 }
